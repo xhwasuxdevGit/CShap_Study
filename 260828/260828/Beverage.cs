4011b7f0105e832fb4772fc523f90a0e1e417304
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-08-28 11:11:59 +0900

    [Feat] 세부 음료메뉴 아메리카노, 카페라뗴 작성

---
+++ 260828/260828/Beverage.cs
@@ -0,0 +1,41 @@
+﻿// 세부 음료 클래스
+
+public class Americano : Beverage
+{
+    private string _name = "아메리카노";
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    
+    public Americano(int itemCost = 2000) : base(itemCost)
+    {
+        _cost = itemCost;
+    }
+    
+    public int Cost
+    {
+        get { return _cost; }
+    }
+}
+
+public class Cafflatte : Beverage
+{
+    private string _name = "카페라떼";
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    
+    public Cafflatte(int itemCost = 3000) : base(itemCost)
+    {
+        _cost = itemCost;
+    }
+    
+    public int Cost
+    {
+        get { return _cost; }
+    }
+}
