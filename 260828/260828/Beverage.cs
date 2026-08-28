8e7da20bfacfdbbcb9c52b609767e3613b04ab4c
xhwasuxdevGit <xhwasuxdev@gmail.com>
2026-08-28 11:15:41 +0900

    [Feat] 음료 클래스 아이스티, 녹차, 쵸코라떼 추가

--- 260828/260828/Beverage.cs
+++ 260828/260828/Beverage.cs
@@ -39,3 +39,63 @@ public class Cafflatte : Beverage
         get { return _cost; }
     }
 }
+
+public class IceTea : Beverage
+{
+    private string _name = "아이스티";
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    
+    public IceTea(int itemCost = 3000) : base(itemCost)
+    {
+        _cost = itemCost;
+    }
+    
+    public int Cost
+    {
+        get { return _cost; }
+    }
+}
+
+public class GreenTea : Beverage
+{
+    private string _name = "아이스티";
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    
+    public GreenTea(int itemCost = 2500) : base(itemCost)
+    {
+        _cost = itemCost;
+    }
+    
+    public int Cost
+    {
+        get { return _cost; }
+    }
+}
+
+public class ChocoLatte : Beverage
+{
+    private string _name = "아이스티";
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    
+    public ChocoLatte(int itemCost = 3500) : base(itemCost)
+    {
+        _cost = itemCost;
+    }
+    
+    public int Cost
+    {
+        get { return _cost; }
+    }
+}
